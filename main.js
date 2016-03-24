1449a224383b4e1186e6f13b033de9789cfa8949
Martin Grossmann <mhgrossmann@yahoo.com>
2016-03-23 19:15:16 -0500

    working on success function

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 //write a JavaScript program that will save an item to a collection when the page loads.  
 
 
-
-
 //need an array with the data to be posted
 var movies = [{'Star Wars': 1977}, {'Raiders of the Lost Ark': 1981}, {'Guardians of the Galaxy': 2014}]
 
+//use forEach
+//put settings in parenthesis
+//use post
 
 var settings = $.ajax{
 
 	url: ' http://small-tiyfe.herokuapp.com/collections/martin-grossmann',
-	type: movies, 
+	type: 'post',
+	data: movies, 
 	datatype: 'jasonp',
-	success: handleData,
+	success: forEach(function(msg)) {
+     $('.answer').html(msg);
+ 	},
+
 	error: function(err) {
 		console.log(err);
 	},
